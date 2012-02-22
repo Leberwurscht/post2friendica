af7b32bfdefd528c7f8af5135b0fd27c201ed1d2
Leberwurscht <leberwurscht@hoegners.de>
2012-02-22 02:02:04 +0100

    more error handling; don't repost images again and again in the same tab

--- data/contentscript.js
+++ data/contentscript.js
@@ -1,12 +1,10 @@
-onMessage = function onMessage(message) {
-    alt = message.alt;
-    src = message.src;
-    
+function insertImage(imagedata)
+{
     // http://stackoverflow.com/questions/2474605/how-to-convert-a-htmlelement-to-a-string
     var container = document.createElement("div");
     var el = document.createElement("img");
-    el.setAttribute('src', src);
-    el.setAttribute('alt', alt);
+    el.setAttribute('src', imagedata.src);
+    el.setAttribute('alt', imagedata.alt);
     container.appendChild(el);
     code = container.innerHTML;
 
@@ -18,4 +16,39 @@ onMessage = function onMessage(message) {
     document.body.removeChild(script);
     
     document.defaultView.postMessage(code, '*');
-;}
+}
+
+self.port.on("post-image", function(imagedata) {
+    try {
+        generator = document.getElementsByName('generator')[0].getAttribute('content');
+    }
+    catch (err) {
+        generator = "";
+    }
+
+    if (generator.substring(0, 9)=="Friendica") {
+        // if this is the login site, wait until login is completed
+        if (document.getElementById("login_standard")) {
+            return;
+        }
+
+        // insert image if text field present
+        if (document.getElementById("profile-jot-text")) {
+            insertImage(imagedata);
+        }
+        else {
+            self.port.emit("notify", {
+                title:"post2friendica was unable to detected a text field",
+                text:"Make sure you supplied an URL ending with /network in the addon preferences."
+            });
+        }
+    }
+    else {
+        self.port.emit("notify", {
+            title:"post2friendica URL error",
+            text:"The URL you specified in the addon preferences does not point to a Friendica site."
+        });
+    }
+
+    self.port.emit("done", true);
+});
